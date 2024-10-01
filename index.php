0e482e8169613527510a737981a308750b4e846c
Milo Adams <adamsl9@mymail.nku.edu>
2024-10-01 11:31:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <html>
-  <--This will be where the user will be welcomed and asked to sign in, if they do not sign in they should only have access to /index.php and /detail.php-->
+  <!--This will be where the user will be welcomed and asked to sign in, if they do not sign in they should only have access to /index.php and /detail.php-->
 </html>
