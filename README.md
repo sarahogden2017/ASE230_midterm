253e138dcc77968ca66e59a21c600c4f00c9c9e4
Sarah Ogden <sarahogden2017@gmail.com>
2024-10-03 10:05:28 -0400

    index.php 1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ASE230_midterm
-Assignment: 8 - Mid-term project
+Assignment: 8 - Mid-term project  
+
+## Project Description  
+Creative writing forum.  
+
+## Bootstrap Theme  
+Journal: https://bootswatch.com/journal/#top
+
--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <html>
   <!--This will be where the user will be welcomed and asked to sign in, if they do not sign in they should only have access to /index.php and /detail.php-->
+  <head>
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/journal/bootstrap.min.css">
+  </head>
+
+  <body>
+    <h1>Creative Writing Forum</h1>
+    <!-- Sign in, sign up, or continue as guest -->
+    <h2>SIGN IN</h2>
+    <form action="" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" value="Sign In">
+    </form>
+    <h2>SIGN UP</h2>
+    <form action="" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" value="Sign Up">
+    </form>
+    <button>CONTINUE AS GUEST</button>
 </html>
