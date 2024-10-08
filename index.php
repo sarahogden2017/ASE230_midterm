c60a8226a50262eb5850ec0f511d7a881e53cb37
Sarah Ogden <sarahogden2017@gmail.com>
2024-10-08 10:24:18 -0400

    login finished

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
+<?php
+  session_start();
+  $message = "";
+  
+  // sign in
+  if (isset($_POST['username_signin']) && isset($_POST['password_signin'])) {
+    $username = $_POST['username_signin'];
+    $password = $_POST['password_signin'];
+    $f = fopen("users.csv", "r");
+    while ($line = fgetcsv($f)) {
+      if ($line[0] == $username && $line[1] == $password) {
+        $_SESSION['username'] = $username;
+        header("Location: ./post/index.php");
+        exit();
+      }
+      elseif ($line[0] == $username && $line[1] != $password) {
+        $message = "Incorrect password.";
+        break;
+      }
+      else {
+        $message = "User not found.";
+      }
+    }
+    fclose($f);
+  }
+
+  // sign up
+  if (isset($_POST['username_signup']) && isset($_POST['password_signup'])) {
+    $username = $_POST['username_signup'];
+    $password = $_POST['password_signup'];
+    $f = fopen("users.csv", "a+");
+    $user_exists = false;
+    while ($line = fgetcsv($f)) {
+      if ($line[0] == $username) {
+        $user_exists = true;
+        $message = "User already exists.";
+        break;
+      }
+    }
+    if (!$user_exists) {
+      fputcsv($f, [$username, $password]);
+      fclose($f);
+      $message = "User created, please sign in.";
+    }
+  }
+
+  // form validation
+  if (isset($_POST['username_signin']) && !isset($_POST['password_signin'])) {
+    $message = "Please enter a password.";
+  }
+  if (isset($_POST['username_signup']) && !isset($_POST['password_signup'])) {
+    $message = "Please enter a password.";
+  }
+  if (isset($_POST['password_signin']) && !isset($_POST['username_signin'])) {
+    $message = "Please enter a username.";
+  }
+  if (isset($_POST['password_signup']) && !isset($_POST['username_signup'])) {
+    $message = "Please enter a username.";
+  }
+
+
+?>
+
 <html>
   <!--This will be where the user will be welcomed and asked to sign in, if they do not sign in they should only have access to /index.php and /detail.php-->
   <head>
@@ -6,19 +69,32 @@
   </head>
 
   <body>
-    <h1>Creative Writing Forum</h1>
-    <!-- Sign in, sign up, or continue as guest -->
-    <h2>SIGN IN</h2>
-    <form action="" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
-      <input type="submit" value="Sign In">
-    </form>
-    <h2>SIGN UP</h2>
-    <form action="" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
-      <input type="submit" value="Sign Up">
-    </form>
-    <button>CONTINUE AS GUEST</button>
+    <div class="d-flex justify-content-center align-items-center">
+      <h1 class="bg-primary text-white p-3 w-100">Creative Writing Forum</h1>
+    </div>
+    <div class="container justify-content-center align-items-center">
+      <!-- Sign in, sign up, or continue as guest -->
+      <h2>SIGN IN</h2>
+      <form action="" method="post">
+        <input type="text" name="username_signin" placeholder="Username">
+        <input type="password" name="password_signin" placeholder="Password">
+        <input type="submit" value="Sign In" class="btn btn-success">
+      </form>
+      <h2>SIGN UP</h2>
+      <form action="" method="post">
+        <input type="text" name="username_signup" placeholder="Username">
+        <input type="password" name="password_signup" placeholder="Password">
+        <input type="submit" value="Sign Up" class="btn btn-success">
+      </form>
+      <button class="btn btn-danger">CONTINUE AS GUEST</button>
+    </div>
+  </body>
+
+  <script>
+    // alert message
+    var message = "<?php echo $message; ?>";
+    if (message) {
+      alert(message);
+    }
+  </script>
 </html>
