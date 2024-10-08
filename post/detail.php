46f188a15e5f05882707a4bb30fe3e58ea4638c8
Sarah Ogden <sarahogden2017@gmail.com>
2024-10-08 10:45:17 -0400

    user permissions

--- post/detail.php
+++ post/detail.php
@@ -11,37 +11,38 @@ function get_entity() {
 ?>
 
 <html>
-<head>
-	<link rel="stylesheet" href="bootstrap.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-  <!--This will be the page that displays the one page that the user has clicked on-->
-  
- <?php
- $post = get_entity();?>
- 
-<body>
-<div class="container text-center">
-	<h2><?=$post[$i]['prompt'];?></h2>
-	<h3>
-	  Prompted by:
-	  <small class="text-body-secondary"><?=$post[$i]['user'];?></small>
-	</h3>
-	 <h4><br /> <br /> The continuous story starts here: <br /></h4>
-	 <p>
-	 <?=$post[$i]['text'];?>
-	 </p>
-	 </div>
- </body>
-<div class="mb-4">
-	<a href="index.php" class="btn btn-primary m-4">Back to prompt index</a>
-</div>
+	<head>
+		<link rel="stylesheet" href="bootstrap.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	</head>
+  	<!--This will be the page that displays the one page that the user has clicked on-->
+  	
+ 	<?php
+ 	$post = get_entity();?>
+ 	
+	<body>
+	<div class="container text-center">
+		<h2><?=$post[$i]['prompt'];?></h2>
+		<h3>
+	  	Prompted by:
+	  	<small class="text-body-secondary"><?=$post[$i]['user'];?></small>
+		</h3>
+	 	<h4><br /> <br /> The continuous story starts here: <br /></h4>
+	 	<p>
+	 	<?=$post[$i]['text'];?>
+	 	</p>
+	 	</div>
+ 	</body>
+	<div class="mb-4">
+		<a href="index.php" class="btn btn-primary m-4">Back to prompt index</a>
+	</div>
 
-<?php
-if($_SESSION['username']!='guest'){
-	?>
-	<a href="edit.php" class="btn btn-primary m-4">Make Edits</a>
-	<a href="delete.php" class="btn btn-primary m-4">Delete Entry</a>
-<?php }?>
+	<?php
+		if (isset($_SESSION['username'])) { ?>
+			<a href="edit.php" class="btn btn-primary m-4">Make Edits</a>
+	<?php } 
+		if($_SESSION['username']==$post[$i]['user']){ ?>
+			<a href="delete.php" class="btn btn-primary m-4">Delete Entry</a>
+	<?php }?>
 
 </html>
